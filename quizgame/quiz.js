dc6daf15aee752fbece41be727d7331f228b5d46
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-07-03 07:39:37 +0530

    Add files via upload

---
+++ quizgame/quiz.js
@@ -0,0 +1,47 @@
+const quizdb=[
+    {
+    question:" Q1: What is the full form of HTML?",
+    a: "Hello To My Land",
+    b:"Hey Text Markup Language",
+    c:"Hypertext Markup Language",
+    d:"Hyper Textmakeup Language",
+    ans:"ans4"
+},
+{
+    question:" Q2: What is the full form of HTML?",
+    a: "Hello To My Land",
+    b:"Hey Text Markup Language",
+    c:"Hypertext Markup Language",
+    d:"Hyper Textmakeup Language",
+    ans:"ans1"
+},
+{
+    question:" Q3: What is the full form of HTML?",
+    a: "Hello To My Land",
+    b:"Hey Text Markup Language",
+    c:"Hypertext Markup Language",
+    d:"Hyper Textmakeup Language",
+    ans:"ans3"
+},
+{
+    question:" Q4: What is the full form of HTML?",
+    a: "Hello To My Land",
+    b:"Hey Text Markup Language",
+    c:"Hypertext Markup Language",
+    d:"Hyper Textmakeup Language",
+    ans:"ans2"
+},
+{
+    question:" Q5: What is the full form of HTML?",
+    a: "Hello To My Land",
+    b:"Hey Text Markup Language",
+    c:"Hypertext Markup Language",
+    d:"Hyper Textmakeup Language",
+    ans:"ans1"
+}];
+const question=document.querySelector('.question');
+const option1=document.querySelector('#option1');
+const option1=document.querySelector('#option2');
+const option1=document.querySelector('#option3');
+const option1=document.querySelector('#option1');
+
