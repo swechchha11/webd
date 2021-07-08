633c56edc7cb16347fdc9e8b05b422a649a64039
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-07-08 21:35:54 +0530

    Update day15.js

--- day15/day15.js
+++ day15/day15.js
@@ -1,1 +1,6 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+ReactDOM.render(<h1>Hello world!!!</h1>,document.getElementById('root'));
