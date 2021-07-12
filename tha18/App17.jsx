4de1c61756986cbc005d6460b50ea0807da102f0
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-07-12 18:16:50 +0530

    Add files via upload

---
+++ tha18/App17.jsx
@@ -0,0 +1,174 @@
+import "./style17.css";
+function App()
+{
+    return(
+        <div className="container">
+    <div className="chess">
+    
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+    
+    
+    </div>
+
+
+<div className="chesso">
+<div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+
+</div>
+
+<div className="chess">
+    
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+    </div>
+
+
+<div className="chesso">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+  
+  
+    
+
+</div>
+
+
+
+<div className="chess">
+    
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+    </div>
+
+
+<div className="chesso">
+<div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+
+</div>
+   
+
+<div className="chess">
+    
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+    </div>
+
+
+<div className="chesso">
+<div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+
+</div>
+   
+<div className="chess">
+    
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+    </div>
+
+
+<div className="chesso">
+<div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    
+
+</div>
+   
+   
+   
+   </div>
+    );
+}
+
+export default App;
---
+++ tha18/index.js
@@ -0,0 +1,31 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App17.jsx";
+
+// ReactDOM.render(<h1>Hello world!!!</h1>,document.getElementById('root'));
+// ReactDOM.render(
+//     <div>
+//          <h1>hello world!!</h1>
+//         <p>welcome to all over here</p>
+//     </div>,
+// document.getElementById("root")
+// );
+
+// function Navbar(){
+//     return <h1>navbar</h1>;
+// }
+
+//function using render
+// function App()
+// {
+//     return(
+
+//     <div>
+//         <Navbar />
+//         <h1>hello world!!</h1>
+//         <p>welcome to all over here</p>
+//     </div>
+// );}
+ReactDOM.render(<App />,document.getElementById("root"));
