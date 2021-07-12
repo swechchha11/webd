fc626b2ba19de2b52b24a55dfaecff3fc4b40e04
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-07-12 17:18:37 +0530

    Add files via upload

---
+++ tha16/App16.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card";
+import "./style16.css";
+
+function App()
+{
+    return(
+        <div className="app">
+            <Card/>
+             {/* <Card/>
+            <Card/>  */}
+        </div>
+        
+  
+        
+);
+}
+
+export default App;
---
+++ tha16/Card.jsx
@@ -0,0 +1,11 @@
+
+
+const Card=() => {
+    return(
+        <div className="card">
+            <h2>I AM ON A SEAFOOD DIET.</h2>
+            <h2 className="bot">I SEE FOOD AND BEGIN TO EAT.</h2>
+        </div>
+    );
+};
+export default Card;
