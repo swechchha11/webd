70f9982448f1667a55cdbbcf17c56fe941cdc6e9
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-08-25 00:53:04 +0530

    Add files via upload

---
+++ day26/Day26App.jsx
@@ -0,0 +1,12 @@
+import "./Day26Apps.css"
+import Add from "./components/Add";
+function App()
+{
+    return(
+        <div className="App">
+            <h1>todo list</h1>
+            <Add/>
+        </div>
+    )
+}
+export default App;
