81659941357ea5c77ae18409881d4afc0c7676a4
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-08-25 00:51:00 +0530

    Add files via upload

---
+++ day27/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Heading from './Heading';
+// import Para from './Para';
+// import List from './List';
+// import Appp from './Appp';
+// import * as zx from './App2'; 
+import App from  "./Day26App.jsx"
+
+
+ReactDOM.render(
+<>
+
+{/* <ol>
+  <li>
+    and
+  </li>
+  <li>{zx.default}</li>
+  <li>{zx.favpro}</li>
+  <li>{zx.myname()}</li>
+  <li>{zx.mynames()}</li>
+</ol> */}
+
+<App></App>
+
+
+</>,document.getElementById('root')
+);
+
+
+
+
+// var React=require('react');
+// var ReactDOM=require('react-dom');
+// ReactDOM.render(<React.StrictMode><App/>
+// </React.StrictMode>,
+//   document.getElementById("root"));
+
