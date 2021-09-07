00f061ce88a7d603a0595672cf08e5803cc0818b
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-09-07 13:13:27 +0530

    Add files via upload

---
+++ backend_day5/day5/index.js
@@ -0,0 +1,7 @@
+const Test = require("./require_test");
+var test = require("./require_test");
+// fs.mkdirSync("day_1");
+// fs.writeFileSync("day_1/hello.txt","Helo");
+
+var obj= new Test();
+console.log(obj.print());
---
+++ backend_day5/day5/require_test.js
@@ -0,0 +1,14 @@
+class Test {
+    print()
+    {
+        console.log("i am inside require_test");
+    }
+}
+
+class Test2{
+    print()
+    {
+        console.log(">>>>>>>");
+    }
+}
+module.exports=Test;
